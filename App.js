0d8a50e706f59a5384a9aab97f740c3c14ce0092
gtchik <gtchik@hotmail.com>
2017-09-01 13:27:01 +0200

    a

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { StyleSheet, Text, View, AppRegistry } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 import  mainActivity  from './Pages/mainActivity.js';
-import {transform} from 'react-transform-hmr'
+
 
 const App = StackNavigator({
   mainActivity: {
